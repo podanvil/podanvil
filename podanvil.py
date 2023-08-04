2274b84236e44705fc225e497af49362cfc45973
Attila Toth <thaodean@gmail.com>
2023-08-04 22:48:55 +0200

    optimize

--- podanvil.py
+++ podanvil.py
@@ -1,45 +1,22 @@
-# This script manages your Docker environment and streamlines the build and deployment of your PaaS infrastructure using Kubernetes.
-# You can run this script without parameters for default behaviour, which will build the Docker image for the project with the tag "podanvil:latest".
-# To build the Docker image with a custom tag, use the '-t' or '--tag' option:
-# python3 podanvil.py --tag my-custom-tag
-# To clean your Docker environment, use the '-c' or '--clean' option:
-# python3 podanvil.py --clean
-
-import argparse
+import os
 import subprocess
+import tarfile
 import sys
-from datetime import datetime
 
-DEFAULT_IMAGE_TAG = "podanvil:latest"
-DEFAULT_REPLICAS = 3
+filename = sys.argv[1]
+directory_name = os.path.splitext(os.path.splitext(filename)[0])[0]  # Remove .tar.gz
 
-def build_image(tag):
-    print(f"Building Docker image with tag: {tag}")
-    subprocess.run(["docker", "build", "-t", tag, "-f", "Dockerfile.rust", "."], check=True)
+with tarfile.open(filename, 'r:gz') as tar:
+    tar.extractall()
 
-def deploy_to_kubernetes(tag, replicas):
-    print(f"Deploying Docker image '{tag}' to Kubernetes with {replicas} replicas")
-    # Kubernetes deployment commands would go here
+subprocess.check_call(["docker", "build", "-t", "podanvil:latest", "."])
+subprocess.check_call(["docker", "tag", "podanvil:latest", "thaodean/podanvil:latest"])
+subprocess.check_call(["docker", "push", "thaodean/podanvil:latest"])
 
-def clean_docker_environment():
-    print("Cleaning Docker environment")
-    # Docker cleanup commands would go here
-
-def main():
-    parser = argparse.ArgumentParser(description="Manage your Docker environment and Kubernetes deployments.")
-    parser.add_argument("-t", "--tag", default=DEFAULT_IMAGE_TAG, help="The tag to use when building the Docker image. Defaults to 'podanvil:latest'.")
-    parser.add_argument("-r", "--replicas", type=int, default=DEFAULT_REPLICAS, help="The number of replicas to use when deploying to Kubernetes. Defaults to 3.")
-    parser.add_argument("-c", "--clean", action='store_true', help="If specified, cleans the Docker environment before building the image and deploying.")
-
-    args = parser.parse_args()
-
-    if args.clean:
-        clean_docker_environment()
-
-    build_image(args.tag)
-
-    deploy_to_kubernetes(args.tag, args.replicas)
-
-if __name__ == "__main__":
-    main()
+# Change this to match your Kubernetes configuration
+subprocess.check_call([
+    "kubectl", "run", "podanvil",
+    "--image=thaodean/podanvil:latest",
+    "--", directory_name
+])
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let directory = args[1].clone();
+    if args.len() != 2 {
+        eprintln!("Usage: {} [directory]", args[0]);
+        std::process::exit(1);
+    }
+    let directory = &args[1];
     let route = warp::fs::dir(directory);
     warp::serve(route).run(([127, 0, 0, 1], 80));
 }
