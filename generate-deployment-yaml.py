eb3b36595f9ffe2f5600511b21f968f1f3afee7e
Attila Toth <thaodean@gmail.com>
2023-08-04 15:27:24 +0200

    optimize

--- generate-deployment-yaml.py
+++ generate-deployment-yaml.py
@@ -1,4 +1,3 @@
-import yaml
 import sys
 
 def convert_to_subdomain(url):
@@ -7,19 +6,15 @@ def convert_to_subdomain(url):
 def main():
     url = sys.argv[1]
     subdomain_safe_url = convert_to_subdomain(url)
-    
-    with open('k8s/ingress_template.yaml', 'r') as f:
-        ingress_yaml = yaml.safe_load(f)
 
-    # Replace occurrences in the YAML
-    ingress_yaml['metadata']['name'] = f"{subdomain_safe_url}-ingress"
-    ingress_yaml['spec']['tls'][0]['hosts'][0] = f"{subdomain_safe_url}.podanvil.com"
-    ingress_yaml['spec']['tls'][0]['secretName'] = f"{subdomain_safe_url}-tls"
-    ingress_yaml['spec']['rules'][0]['host'] = f"{subdomain_safe_url}.podanvil.com"
-    ingress_yaml['spec']['rules'][0]['http']['paths'][0]['backend']['service']['name'] = f"{subdomain_safe_url}-service"
+    with open('k8s/ingress_template.yaml', 'r') as f:
+        ingress_yaml = f.read()
+
+    # Replace placeholders in the YAML
+    ingress_yaml = ingress_yaml.replace('{{subdomain_safe_url}}', subdomain_safe_url)
 
     with open(f'k8s/{subdomain_safe_url}_ingress.yaml', 'w') as f:
-        yaml.dump(ingress_yaml, f)
+        f.write(ingress_yaml)
 
 if __name__ == "__main__":
     main()
